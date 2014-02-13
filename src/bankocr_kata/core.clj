bcc0345b3cdf3fafe37c695cc5ac2adbf45e5d4b
Tim McIver <tim@timmciver.com>
2014-02-13 08:47:13 -0500

    Started writing main.

--- src/bankocr_kata/core.clj
+++ src/bankocr_kata/core.clj
@@ -1,2 +1,11 @@
-(ns bankocr-kata.core)
+(ns bankocr-kata.core
+  (:require [clojure.java.io :as io]))
 
+(defn -main
+  [path]
+  (with-open [rdr (io/reader path)
+              wrtr (io/writer (str path ".out"))]
+    (let [account-numbers (parse rdr)]
+      (doseq [account-number account-numbers]
+        (.write wrtr account-number)
+        (.write wrtr '\n')))))
